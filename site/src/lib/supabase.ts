47911697e544381a591ff660fd67d3bb44f1e89f
smitharajasenan <smitha.r.santhosh@gmail.com>
2026-04-13 09:59:51 -0400

    Changes

---
+++ site/src/lib/supabase.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+export const supabase = createClient(
+  'https://bppzlhrzoqzqgvechswu.supabase.co',
+  'sb_publishable_04YT8xHyWVaml448snDw3g_uTVD9Nid'
+);
